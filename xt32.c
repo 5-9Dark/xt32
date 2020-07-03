7122d032aa1db3f401e0588ad021b7d6a44857ea
CRO-THEHACKER <crothehacker@fiveninedark.com>
2020-07-03 03:04:30 -0400

    Update xt32.c

--- xt32.c
+++ xt32.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
-int main() {
-   /* */
-   
-   return 0;
+int main( int argc, char *argv[] ) {
+
+   if( argc == 2 ) {
+      printf("The argument supplied is %s\n", argv[1]);
+   }
+   if( argc == "help" ) {
+      printf("xt32: Test.\n");
+   }
+   else {
+      printf("xt32: Argument not expected...\n\n");
+   }
+
+
 }
