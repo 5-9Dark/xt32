c5c3b1aaac66a8e8a3ab2fdc2a73d92d573036f1
encrypted23 <56136248+encrypted23@users.noreply.github.com>
2020-07-06 23:48:03 -0400

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
 int main(){
-    
+
+    str_encode();
 
     return 0;
 }
 
-void encode(){
+void str_encode(){
+    char string[100];
+    char key[100];
+    printf("[+] String to encode: ");
+    scanf("%c", &string);
 
 }
 
-void decode(){
 
-
-}
