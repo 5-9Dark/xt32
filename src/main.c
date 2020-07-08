9d34fd3b9a2eee6ccc6b17eed4ef7b8783675b13
ajhblu <57992230+AJHblu@users.noreply.github.com>
2020-07-08 11:04:38 -0400

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
+#include <string.h>
 /* Made by: Encrypted, leon and AJblu */
+char alphabet[86] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3',
+    '4', '5', '6', '7', '8', '9', '0'
+    };
+
+int leng;
 
 int main(){
 
+
+
     int input;
 
-    printf("[---------- Welcome to the xt32 encoder by 5/9Dark. ------------]\n|                                                               |\n|   * Make sure to set shift!                                   |\n|                                                               |\n|                                                               |\n|             1.) Encode              2.) Decode                |\n|                                                               |\n|                  [ (C) 2020 FiveNineDark ]                    |\n[---------------------------------------------------------------]\n\n\n\n");
+    printf("[---------- Welcome to the xt32 encoder by 5/9Dark. ------------]\n|                                                               |\n|                                                               |\n|                                                               |\n|                                                               |\n|             1.) Encode              2.) Decode                |\n|                  * Make sure to set shift!                    |\n|                  [ (C) 2020 FiveNineDark ]                    |\n[---------------------------------------------------------------]\n\n\n\n");
+
     printf("[+] Select your option: ");
-    scanf("%d", &input);
+    scanf("%d%*c", &input);
 
     if(input == 1){
         str_encode();
@@ -25,9 +35,19 @@ void str_encode(){
     char key[100];
 
     printf("[+] String to encode: ");
-    scanf("%d", &string);
+    fgets(string, 100, stdin);
     printf("[+] Key to string: ");
-    scanf(" %d", &key);
+    fgets(key, 100, stdin);
+
+    for(int i = 0; i < 86; i++){
+        leng = strlen(key);
+        char lett = alphabet[i];
+        printf("%c", lett);
+        for(int x = 0; x < leng; x++){
+        //if key[x] == lett{
+        //do stuff}
+        };
+    };
 
 }
 
@@ -36,8 +56,8 @@ void str_decode(){
     int key_dec[100];
 
     printf("[+] Enter string to decode: ");
-    scanf("%d", &string_dec);
+    fgets(string_dec, 100, stdin);
     printf("[+] Key to string: ");
-    scanf(" %d", &key_dec);
+    fgets(key_dec, 100, stdin);
 
 }
